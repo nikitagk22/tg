2c03e4969bcbfb69285820e7d68f7e7fea96a8b3
nikitagk22 <150912285+nikitagk22@users.noreply.github.com>
2025-08-12 13:27:23 +0300

    Update userbot_config.py

--- userbot_config.py
+++ userbot_config.py
@@ -2,14 +2,14 @@
 # Получите эти данные на https://my.telegram.org/
 
 # API ID и Hash от Telegram
-API_ID = 26238390 # Замените на ваш API ID
-API_HASH = "ab74dca7373ba12e9c7f8d3054f9f149"  # Замените на ваш API Hash
+API_ID =  # Замените на ваш API ID
+API_HASH = ""  # Замените на ваш API Hash
 
 # Номер телефона в международном формате
-PHONE = "+77020648560"  # Замените на ваш номер
+PHONE = ""  # Замените на ваш номер
 
 # Токен вашего основного бота
-BOT_TOKEN = "7794286590:AAEZphG6ODyyIMfUhM2aREQZcsfAcpuh__s"  # Замените на токен вашего бота
+BOT_TOKEN = ""  # Замените на токен вашего бота
 
 # Примечание: 
 # 1. API_ID и API_HASH получаются на https://my.telegram.org/
